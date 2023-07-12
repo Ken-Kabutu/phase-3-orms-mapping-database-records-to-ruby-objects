b4d7052398bdd54e75c0c78a2a080f689a6ff2ed
Ken <kabutukenneth3@gmail.com>
2023-07-12 20:26:31 +0300

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,33 @@ class Song
     song.save
   end
 
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.new_from_db(row)
+    id, name, album = row
+    Song.new(name: name, album: album, id: id)
+  end
+
 end
